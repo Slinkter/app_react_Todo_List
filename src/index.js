5a4e93b1da126286d2718c5214161ec25db27d96
jhonnycave <luis.j.cueva@gmail.com>
2022-03-11 19:40:51 -0500

    borrar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App >
+     <h1> Presupuesto </h1>
+    </App>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 reportWebVitals();
