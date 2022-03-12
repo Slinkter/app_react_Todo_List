dbf4a26cbf4ab71de713b3c94738f19571171484
jhonnycave <luis.j.cueva@gmail.com>
2022-03-11 20:34:08 -0500

    12/22:fin

--- src/App/index.js
+++ src/App/index.js
@@ -4,18 +4,29 @@ import { AppUI } from "./AppUI";
 
 // data externa
 
-const defaultTODOs = [
-  { text: "Cortar cebolla", completed: true },
-  { text: "Tomar clases de ingles", completed: false },
-  { text: "llorara con al llorona", completed: false },
-  { text: "Curso Fisica", completed: false },
-  { text: "Curso Node", completed: true },
-];
+// const defaultTODOs = [
+//   { text: "Cortar cebolla", completed: true },
+//   { text: "Tomar clases de ingles", completed: false },
+//   { text: "llorara con al llorona", completed: false },
+//   { text: "Curso Fisica", completed: false },
+//   { text: "Curso Node", completed: true },
+// ];
 
 // importacion de components
 
 function App() {
-  const [stateTodos, setStateTodos] = React.useState(defaultTODOs);
+  const localStorageTodos = localStorage.getItem("TODOS_V1");
+
+  let arrayDefaultParsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem("TODOS_V1", JSON.stringify([]))
+    arrayDefaultParsedTodos = []
+  } else {
+    arrayDefaultParsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [stateTodos, setStateTodos] = React.useState(arrayDefaultParsedTodos);
   const [stateSearch, setStateSearch] = React.useState("");
 
   const count_TotalTodos = stateTodos.length;
@@ -35,21 +46,29 @@ function App() {
     });
   }
 
+  //LOCALSTORAGE
+  const saveTodos = (newTodos) =>{
+    const stringifyTodos = JSON.stringify(newTodos)
+    localStorage.setItem('TODOS_V1',stringifyTodos)
+    setStateTodos(newTodos)
+  }
+
+
+
   //metodo , cambiar de false a true
   const onUpdateItem = (text) => {
     const index = stateTodos.findIndex((item) => item.text === text); // si coincide el text ,coger index , sale un numero
     const newTodos = [...stateTodos]; // copy array
     newTodos[index].completed = true; //cambiar a true
-    setStateTodos(newTodos);
-    console.log(index);
-    console.log(newTodos);
+    saveTodos(newTodos);
+   
   };
 
   const onDeleteItem = (text) => {
     const index = stateTodos.findIndex((item) => item.text === text); // si coincide el text ,coger
     const newTodos = [...stateTodos];
     newTodos.splice(index, 1);
-    setStateTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
-<<<<<<< HEAD
-import reportWebVitals from "./reportWebVitals";
-
-
-
-
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App >
-     <h1> Presupuesto </h1>
-    </App>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-reportWebVitals();
-=======
 
 ReactDOM.render(<App />, document.getElementById("root"));
->>>>>>> 0001dcb7f4b0f9c5afc5d89c4c6ea5693ebba427
